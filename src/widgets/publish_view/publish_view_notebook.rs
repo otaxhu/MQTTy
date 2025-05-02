8a8bf77e49fce73039d0ef4772d2f01d6f3a5fc0
otaxhu <oscarperniamoreno@gmail.com>
2025-05-02 16:08:33 -0400

    fix(publish-view internals): Using enum values for mqtt_version and qos properties

--- src/widgets/publish_view/publish_view_notebook.rs
+++ src/widgets/publish_view/publish_view_notebook.rs
@@ -17,7 +17,7 @@ use std::cell::{Cell, OnceCell, RefCell};
 
 use adw::prelude::*;
 use adw::subclass::prelude::*;
-use gtk::glib;
+use gtk::{gio, glib};
 
 use crate::client::{MQTTyClient, MQTTyClientMessage, MQTTyClientQos, MQTTyClientVersion};
 use crate::content_type::MQTTyContentType;
@@ -38,9 +38,8 @@ mod imp {
         #[property(get, set, override_interface = MQTTyDisplayModeIface)]
         display_mode: Cell<MQTTyDisplayMode>,
 
-        // TODO: Use enum values from client module
-        #[property(get, set)]
-        mqtt_version: RefCell<String>,
+        #[property(get, set, builder(Default::default()))]
+        mqtt_version: Cell<MQTTyClientVersion>,
 
         #[property(get, set)]
         topic: RefCell<String>,
@@ -48,9 +47,8 @@ mod imp {
         #[property(get, set)]
         url: RefCell<String>,
 
-        // TODO: Use enum values from client module
-        #[property(get, set)]
-        qos: RefCell<String>,
+        #[property(get, set, builder(Default::default()))]
+        qos: Cell<MQTTyClientQos>,
 
         #[property(get, set)]
         body: RefCell<String>,
@@ -66,6 +64,9 @@ mod imp {
 
         #[template_child]
         pub user_properties_tab: TemplateChild<MQTTyPublishUserPropsTab>,
+
+        #[template_child]
+        user_properties_stack: TemplateChild<gtk::Stack>,
     }
 
     impl Default for MQTTyPublishViewNotebook {
@@ -82,6 +83,7 @@ mod imp {
                 user_properties_tab: Default::default(),
                 username: Default::default(),
                 password: Default::default(),
+                user_properties_stack: Default::default(),
             }
         }
     }
@@ -97,8 +99,6 @@ mod imp {
         type Interfaces = (MQTTyDisplayModeIface,);
 
         fn class_init(klass: &mut Self::Class) {
-            klass.install_property_action("publish-view-notebook.mqtt-version", "mqtt_version");
-            klass.install_property_action("publish-view-notebook.qos", "qos");
             klass.bind_template();
         }
 
@@ -108,7 +108,81 @@ mod imp {
     }
 
     #[glib::derived_properties]
-    impl ObjectImpl for MQTTyPublishViewNotebook {}
+    impl ObjectImpl for MQTTyPublishViewNotebook {
+        fn constructed(&self) {
+            self.parent_constructed();
+
+            let obj = self.obj();
+
+            let group = gio::SimpleActionGroup::new();
+
+            let mqtt_version_state = gio::SimpleAction::new_stateful(
+                "mqtt-version",
+                Some(glib::VariantTy::STRING),
+                &"3".into(),
+            );
+            mqtt_version_state
+                .bind_property("state", &*obj, "mqtt_version")
+                .bidirectional()
+                .sync_create()
+                .transform_to(|_, state: glib::Variant| {
+                    let version = match state.str().unwrap() {
+                        "3" => MQTTyClientVersion::V3X,
+                        "5" => MQTTyClientVersion::V5,
+                        version => panic!("invalid MQTT version: {version}"),
+                    };
+
+                    Some(version)
+                })
+                .transform_from(|_, mqtt_version: MQTTyClientVersion| {
+                    let new_state = match mqtt_version {
+                        MQTTyClientVersion::V3X => "3",
+                        MQTTyClientVersion::V5 => "5",
+                    };
+
+                    Some(glib::Variant::from(new_state))
+                })
+                .build();
+
+            let qos_state =
+                gio::SimpleAction::new_stateful("qos", Some(glib::VariantTy::STRING), &"0".into());
+            qos_state
+                .bind_property("state", &*obj, "qos")
+                .bidirectional()
+                .sync_create()
+                .transform_to(|_, state: glib::Variant| {
+                    let qos = match state.str().unwrap() {
+                        "0" => MQTTyClientQos::Qos0,
+                        "1" => MQTTyClientQos::Qos1,
+                        "2" => MQTTyClientQos::Qos2,
+                        qos => panic!("invalid MQTT QoS: {qos}"),
+                    };
+
+                    Some(qos)
+                })
+                .transform_from(|_, qos: MQTTyClientQos| {
+                    let new_state = match qos {
+                        MQTTyClientQos::Qos0 => "0",
+                        MQTTyClientQos::Qos1 => "1",
+                        MQTTyClientQos::Qos2 => "2",
+                    };
+
+                    Some(glib::Variant::from(new_state))
+                })
+                .build();
+
+            group.add_action(&mqtt_version_state);
+            group.add_action(&qos_state);
+
+            obj.insert_action_group("publish-view-notebook", Some(&group));
+
+            mqtt_version_state
+                .bind_property("state", &*self.user_properties_stack, "visible-child-name")
+                .transform_to(|_, state: glib::Variant| state.str().map(String::from))
+                .sync_create()
+                .build();
+        }
+    }
     impl WidgetImpl for MQTTyPublishViewNotebook {}
     impl BinImpl for MQTTyPublishViewNotebook {}
     impl MQTTyDisplayModeIfaceImpl for MQTTyPublishViewNotebook {}
@@ -126,11 +200,7 @@ impl MQTTyPublishViewNotebook {
     }
 
     pub async fn send(&self) -> Result<(), String> {
-        let mqtt_version = match self.mqtt_version().as_ref() {
-            "3" => MQTTyClientVersion::V3X,
-            "5" => MQTTyClientVersion::V5,
-            ver => panic!("Invalid MQTT version: {ver}"),
-        };
+        let mqtt_version = self.mqtt_version();
 
         let client = MQTTyClient::new(
             &self.url(),
@@ -144,12 +214,7 @@ impl MQTTyPublishViewNotebook {
         let msg = MQTTyClientMessage::new();
 
         msg.set_topic(self.topic());
-        msg.set_qos(match self.qos().as_ref() {
-            "0" => MQTTyClientQos::Qos0,
-            "1" => MQTTyClientQos::Qos1,
-            "2" => MQTTyClientQos::Qos2,
-            qos => panic!("Invalid Qos level: {qos}"),
-        });
+        msg.set_qos(self.qos());
         if self.content_type() != MQTTyContentType::None {
             msg.set_body(self.body().as_ref());
         }
