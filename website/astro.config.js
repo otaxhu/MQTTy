22469a82955f78647d0763d7f7f4fc9458566832
otaxhu <oscarperniamoreno@gmail.com>
2025-08-15 00:36:43 -0400

    feat(website): Github workflow for generating and deploying the landing page

--- website/astro.config.js
+++ website/astro.config.js
@@ -3,6 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   srcDir: ".",
+  // Fill this environment variable when building and deploying the site
+  base: process.env.ASTRO_CONFIG_BASE,
   vite: {
     plugins: [tailwindcss()],
   },
